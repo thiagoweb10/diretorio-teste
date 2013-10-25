58396261813395e3880eb790aff3274363f17206
thiagoweb10 <thiago_melo18@yahoo.com.br>
2013-10-24 23:21:14 -0200

    foi modificado o testo de bem vindos do arquivo. Signed-off-by: thiagoweb10 <thiago_melo18@yahoo.com.br>

--- main.php
+++ main.php
@@ -1,4 +1,7 @@
 <?php
 $_ge['usuario'];
-     echo "Bem vindo usuario ".$_get['usuario'];
+     echo "Bem vindo ao nosso site usuario ".$_get['usuario'];
+
+     include("conexao/conexao.php");
+     echo "testando para ver se deu certo!!";
 ?>
